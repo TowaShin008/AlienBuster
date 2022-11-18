83698ec15c7cfd092330b6ba789f63adb3a872e8
LE4B_YAMAGUCHI_RYUTO <k019g1244@g.neec.ac.jp>
2022-11-18 15:48:30 +0900

    エンドシーンにタイトルに促すUI追加

--- Assets/EndScene.cs
+++ Assets/EndScene.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class EndScene : MonoBehaviour
 {
     // Start is called before the first frame update
+    int timer;
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (timer > 120)
         {
-            FadeManager.Instance.LoadScene("TitleScene", 0.5f);
+            if (Input.GetKeyDown("space"))
+            {
+                FadeManager.Instance.LoadScene("TitleScene", 0.5f);
+            }
         }
+        else
+        {
+            timer++;
+        }
+      
     }
 }
