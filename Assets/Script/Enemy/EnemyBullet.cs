57e753341a365499a8bfd73410b4d3b5fe8d4e15
TowaShin008 <87635482+TowaShin008@users.noreply.github.com>
2022-11-19 10:25:30 +0900

    マージ後の修正。

--- Assets/Script/Enemy/EnemyBullet.cs
+++ Assets/Script/Enemy/EnemyBullet.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     // Start is called before the first frame update
+    int timer;
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD:Assets/Script/Enemy/EnemyBullet.cs
         
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
+=======
+        if (timer > 120)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                FadeManager.Instance.LoadScene("TitleScene", 0.5f);
+            }
+        }
+        else
+        {
+            timer++;
+        }
+      
+>>>>>>> origin/yamaguchi:Assets/EndScene.cs
     }
 }
--- Assets/Script/TitleSceneScript.cs
+++ Assets/Script/TitleSceneScript.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class TitleSceneScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //タイトルのclickアニメーション
+    [SerializeField]
+    Animator animator;
+    float speed = 1f;
+    bool click = false;
     void Start()
     {
-        
+        click = false;
+        speed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD:Assets/Script/TitleSceneScript.cs
         //if (Input.GetKeyDown("space"))
         //{
         //    FadeManager.Instance.LoadScene("GameScene", 0.5f);
         //}
+=======
+        if (Input.GetKeyDown("space"))
+        {
+            speed += 10;
+            animator.SetFloat("Speed", speed);
+            FadeManager.Instance.LoadScene("GameScene", 1f);
+        }
+>>>>>>> origin/yamaguchi:Assets/TitleSceneScript.cs
     }
 }
