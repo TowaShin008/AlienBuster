79a7119caad6b2306c54461dbfee74552dce3b1c
TowaShin008 <87635482+TowaShin008@users.noreply.github.com>
2022-12-02 13:25:03 +0900

    武器ドロップの改良。マージ後の修正。
    前回、ステージの下のほうにあった武器アイテムをなくした。マージ後のアタッチが外れている部分を修正した。

--- Assets/Script/Item/SniperRifleItem.cs
+++ Assets/Script/Item/SniperRifleItem.cs
@@ -11,7 +11,7 @@ public class SniperRifleItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
