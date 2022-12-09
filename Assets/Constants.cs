c121994d31859d0152f325f0e56e99e4c8de3a44
TowaShin008 <87635482+TowaShin008@users.noreply.github.com>
2022-12-09 11:10:45 +0900

    敵モデルの適用。不要ファイルの削除。
    敵モデルをキューブからメンダコのようなものに変え文字化けしている銃の効果音のファイルを削除した。

--- Assets/Constants.cs
+++ Assets/Constants.cs
@@ -16,9 +16,11 @@ namespace Util
 		public static float stageMaxPositionX = 465.0f;
 		//ステージのX軸の最小値
 		public static float stageMinPositionX = -1791.0f;
-
+		//通常弾のダメージ量
 		public static int normalBulletDamage = 1;
+		//スナイパーライフルのダメージ量
 		public static int sniperBulletDamage = 10;
+		//ロケット弾のダメージ量
 		public static int rocketBombDamage = 10;
 	}
 }
